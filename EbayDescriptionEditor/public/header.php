85e2a7d219e7b06b638b68d8a9d5292d86491ede
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-01-03 13:37:35 +0000

    Created basic text editor

--- EbayDescriptionEditor/public/header.php
+++ EbayDescriptionEditor/public/header.php
@@ -5,8 +5,11 @@
 <head>
     <meta charset="UTF-8">
     <link href="../assets/css/SiteStyle.css" rel="stylesheet" type="text/css">
-    <script src="https://kit.fontawesome.com/fca550da2e.js" crossorigin="anonymous"></script>
-    <title>Linked data app</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <script src="../assets/JavaScript/app.js"></script>
+
+<!--    <script src="https://kit.fontawesome.com/fca550da2e.js" crossorigin="anonymous"></script>-->
+    <title>Text Editor</title>
 </head>
 <header id="myBanner" role="banner" class="theBanner">
 
--- EbayDescriptionEditor/public/index.php
+++ EbayDescriptionEditor/public/index.php
@@ -8,6 +8,13 @@ include_once "header.php";
 <h2>Editor</h2>
 <p>enter your text here</p>
 
-<input type="text" name="n1">
+<div class="sample-toolbar">
+    <a href="javascript:void(0)" onclick="format('bold')"><span class="fas fa-bold" style="color: black"></span></a>
+    <a href="javascript:void(0)" onclick="format('italic')"><span class="fa fa-italic fa-fw" style="color: black"></span></a>
+    <a href="javascript:void(0)" onclick="format('insertunorderedlist')"><span class="fa fa-list fa-fw" style="color: black"></span></a>
 
+</div>
+
+<div class="editor" contenteditable="true">
+</div>
 </body>
